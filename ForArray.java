36909d5536f3a6a0c99ff749fc3c145e27776952
mleers <mleers4@gmail.com>
2018-09-26 11:18:02 -0700

    Add files via upload

---
+++ ForArray.java
@@ -0,0 +1,14 @@
+
+public class ForArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] alpha = 
+			{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+			
+
+		for (int item: alpha) {
+			System.out.printf("The alphabet is: %c \n",item);
+		}
+
+}}
