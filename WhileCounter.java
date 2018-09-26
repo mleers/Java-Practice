06212bfee9a57073a2cc40908a19c2be27ec17ec
mleers <mleers4@gmail.com>
2018-09-26 11:17:39 -0700

    Add files via upload

---
+++ WhileCounter.java
@@ -0,0 +1,14 @@
+
+public class WhileCounter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 1;
+		
+		while (count < 11) {
+			System.out.println("Count is: " + count);
+			count++;
+	}
+
+}}
+
